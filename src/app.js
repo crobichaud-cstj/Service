7e624f63988cd4cc13ac77edad7ad641c8a60f29
Cedrick Robichaud <2150658@cstj.qc.ca>
2022-09-01 14:59:58 -0400

    asd

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+import dayjs from "dayjs";
 import express from "express";
 
 const app = express();
 
-
+//TODO: Ajouter code ici
 
 app.get("/premiere", (req, res) => {
     res.status(200)
@@ -10,4 +11,37 @@ app.get("/premiere", (req, res) => {
     res.send("Première route avec express");
 })
 
+app.get("/date", (req, res) => {
+    res.status(200)
+    res.set("Content-Type", "text/plain");
+    const today = dayjs();
+    res.send(today.format());
+})
+
+app.get("/maths/:operation", (req, res) => {
+    const a = parseInt(req.query.a, 10);
+    const b = parseInt(req.query.b, 10);
+    const operation = req.params.operation
+    res.status(200)
+    res.set("Content-Type", "text/plain");
+
+    switch (operation) {
+        case "somme":
+            res.send((a + b).toString());
+            break;
+        case "quotient":
+            res.send((a / b).toString());
+            break;
+        case "produit":
+            res.send((a * b).toString());
+            break;
+        case "difference":
+            res.send((a - b).toString());
+            break;
+        default:
+            res.send("Erreur");
+    }
+
+
+});
 export default app;
